fed2932abe88a9259eff075724d2c23d01060cf0
sankalp <sankalpkashiv0@gmail.com>
2025-04-20 22:02:10 +0530

    new start

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "postgresql+psycopg2://postgres:Kashiv%40882@localhost:5432/authdb"
+
 DATABASE_URL = "postgresql://postgres_feed_parser_user:SwEDl3mYGvi7ZYXxQgTnYAVidpAThKvo@dpg-cvvo2di4d50c739iov00-a/postgres_feed_parser"
 
 engine = create_engine(DATABASE_URL)
